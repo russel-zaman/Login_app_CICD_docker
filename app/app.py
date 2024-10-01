51567f25d4048b2b10d00bf966da8aae85979fd8
russel-zaman <kazi_366953@gmx.de>
2024-10-01 16:42:10 +0200

    changed the port in DC fiel

--- app/app.py
+++ app/app.py
@@ -91,4 +91,4 @@ def register():
     return render_template('register.html', msg=msg)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
